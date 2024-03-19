b43b9ea26943b7abdbaff4e6963a269115692d80
Leonid Andronov <andronovl@users.noreply.github.com>
2024-03-19 16:19:47 -0700

    Update shiftXYlinlast.m

--- SharpViSu/shiftXYlinlast.m
+++ SharpViSu/shiftXYlinlast.m
@@ -1,4 +1,4 @@
-function [shiftx, shifty] = shiftXYlinlast(A, B, frames)
+function [shiftx, shifty, shiftz] = shiftXYlinlast(A, B, frames)
 
 f = waitbar(0, 'Pairing...');
 A1n = cell(A(end,2)-A(1,2)+1,1);
@@ -28,12 +28,13 @@ end
 %
 shiftx = cell(size(A1n));
 shifty = cell(size(A1n));
+shiftz = cell(size(A1n));
 for i = 1:size(A1n,1)
     if ~isempty(A1n{i}) && ~isempty(B1n{i})
-        [~,Idx] = pdist2(B1n{i}(:,4), A1n{i}(:,4),'euclidean','Smallest',1);
-        [~,Idy] = pdist2(B1n{i}(:,5), A1n{i}(:,5),'euclidean','Smallest',1);
-        shiftx{i} = B1n{i}(Idx, 4) - A1n{i}(:,4);
-        shifty{i} = B1n{i}(Idy, 5) - A1n{i}(:,5);
+        [~,I] = pdist2(B1n{i}(:,4:6), A1n{i}(:,4:6), 'euclidean', 'Smallest', 1);
+        shiftx{i} = B1n{i}(I, 4) - A1n{i}(:,4);
+        shifty{i} = B1n{i}(I, 5) - A1n{i}(:,5);
+        shiftz{i} = B1n{i}(I, 6) - A1n{i}(:,6);
     end
     if mod(i,5000) == 0
         waitbar(2/3 + i/size(A1n,1)/3,f);
@@ -41,4 +42,5 @@ for i = 1:size(A1n,1)
 end
 shiftx = cell2mat(shiftx);
 shifty = cell2mat(shifty);
+shiftz = cell2mat(shiftz);
 delete(f);
